v0.1, added checkpoint averaging, fixed data iterators, adjusted hyperparameters, improved bugs

codebase/attention_modules.py
@@ -155,7 +155,7 @@ class MultiHeadAttentionLayer(object):
         weighted_memories = tf.matmul(attn_weights, values)
         return weighted_memories
 
-    def forward(self, query_context, memory_context, attn_mask, layer_memories):
+    def forward(self, query_context, memory_context, attn_mask, layer_memories, global_context=None):
         """ Propagates the input information through the attention layer. """
         # The context for the query and the referenced memory is identical in case of self-attention
         if memory_context is None:
@@ -182,6 +182,7 @@ class MultiHeadAttentionLayer(object):
         weighted_memories = self._merge_from_heads(split_weighted_memories)
         # Feed through a dense layer
         projected_memories = self.context_projection.forward(weighted_memories)
+
         return projected_memories, layer_memories
 
 
@@ -413,45 +414,4 @@ class FineGrainedAttentionLayer(SingleHeadAttentionLayer):
         expanded_values = tf.expand_dims(values, axis=1)
         weighted_memories = \
             tf.reduce_sum(tf.multiply(tf.transpose(attn_weights, [1, 0, 2, 3]), expanded_values), axis=2)
-        return weighted_memories
-
-    def _attn(self, queries, keys, values, attn_mask):
-        """ For each encoder layer, weighs and combines time-step-wise hidden representation into a single layer
-        context state.  -- DEPRECATED, SINCE IT'S SLOW AND PROBABLY NOT ENTIRELY CORRECT """
-        # Account for beam-search
-        num_beams = tf.shape(queries)[0] // tf.shape(keys)[0]
-        keys = tf.tile(keys, [num_beams, 1, 1])
-        values = tf.tile(values, [num_beams, 1, 1])
-
-        def _logits_fn(query):
-            """ Computes position-wise attention scores. """
-            query = tf.expand_dims(query, 1)
-            # return tf.squeeze(self.attn_weight * (tf.nn.tanh(keys + query + norm_bias)), axis=2)
-            return self.attn_weight * tf.nn.tanh(keys + query)  # 4D output
-
-        def _weighting_fn(step_weights):
-            """ Computes position-wise context vectors. """
-            # step_weights = tf.expand_dims(step_weights, 2)
-            return tf.reduce_sum(tf.multiply(step_weights, values), axis=1)
-
-        # Obtain attention scores
-        transposed_queries = tf.transpose(queries, [1, 0, 2])
-        attn_logits = tf.map_fn(_logits_fn, transposed_queries)  # multiple queries per step are possible
-        if attn_mask is not None:
-            # attn_logits has shape=[batch, query_lengh, key_length, attn_features]
-            transposed_mask = \
-                tf.transpose(tf.tile(attn_mask, [tf.shape(queries)[0] // tf.shape(attn_mask)[0], 1, 1, 1]),
-                             [2, 0, 3, 1])
-            attn_logits += transposed_mask
-
-        # Compute the attention weights
-        attn_weights = tf.nn.softmax(attn_logits, axis=-2, name='attn_weights')
-
-        # Optionally apply dropout
-        if self.dropout_attn > 0.0:
-            attn_weights = tf.layers.dropout(attn_weights, rate=self.dropout_attn, training=self.training)
-
-        # Obtain context vectors
-        weighted_memories = tf.map_fn(_weighting_fn, attn_weights)
-        weighted_memories = tf.transpose(weighted_memories, [1, 0, 2])
         return weighted_memories

codebase/custom_iterator.py
@@ -39,7 +39,7 @@ def shuffle(files, exp_path, temporary=False):
         shuffled_files = []
         for file_path in files:
             _, filename = os.path.split(os.path.realpath(file_path))
-            shuffled_files.\
+            shuffled_files. \
                 append(tempfile.NamedTemporaryFile(mode='w+', prefix='{:s}.shuf'.format(filename), dir=exp_path))
     else:
         shuffled_files = [open('{:s}.shuf'.format(file_path), 'w') for file_path in files]
@@ -55,6 +55,25 @@ def shuffle(files, exp_path, temporary=False):
         [file.close() for file in shuffled_files]
 
     return shuffled_files
+
+
+def get_document_length(fileobject):
+    """ Fast way to compute the length of a document;
+     See: https://stackoverflow.com/questions/845058/how-to-get-line-count-cheaply-in-python """
+    # Set up source file and buffer
+    file = open(fileobject, 'rb')
+    lines = 0
+    buffer_size = 1024 * 1024
+    read_file = file.raw.read
+
+    # Count lines
+    buffer = read_file(buffer_size)
+    while buffer:
+        lines += buffer.count(b'\n')
+        buffer = read_file(buffer_size)
+    file.close()
+
+    return lines
 
 
 class TextIterator(object):
@@ -79,6 +98,8 @@ class TextIterator(object):
                  shuffle_each_epoch=False,
                  training=False):
 
+        self.config = config
+
         self.source_path = source_path
         self.target_path = target_path
         self.exp_path = exp_path
@@ -92,6 +113,10 @@ class TextIterator(object):
                 self.target = fopen(target_path, 'r')
             else:
                 self.target = None
+
+        # Get file length (only needed when using multiple GPUs)
+        self.num_lines = get_document_length(self.source_path) if self.config.num_gpus > 1 else 0
+        self.all_lines_read = False
 
         # Handle dictionaries
         self.source_dicts = source_dicts
@@ -119,15 +144,24 @@ class TextIterator(object):
         else:
             self.batch_size = sentence_batch_size
             self.token_batches = False
+
         self.maxibatch_size = \
-            (self.batch_size * config.maxibatch_size) if config.maxibatch_size > 0 else self.batch_size
+            (self.batch_size * self.config.maxibatch_size) if config.maxibatch_size > 0 else self.batch_size
+
+        # For multi-GPU training/ inference, keep track of items remaining in a maxi-batch as well as the remaining
+        # GPUs in the current GPU cycle
+        self.curr_maxibatch_size = 0
+        self.gpu_tracker = 0
 
         # Shrink batch size to accommodate beam search
         if not training:
             if config.translate_only and not config.translate_with_beam_search:
                 pass
             else:
-                self.batch_size = self.batch_size // max(1, np.floor(config.beam_size / 3))  # TODO: hack; needed?
+                # TODO: Should be adjusted vor CE-validation only, i.e. no diminishing
+                # TODO: Would require two different iterators for CE-validation and BLEU-validation
+                # self.batch_size = self.batch_size // max(1, np.floor(config.beam_size / 3))  # TODO: hack; needed?
+                self.batch_size = max(1, self.batch_size // config.beam_size)  # TODO: hack; needed?
 
         self.time_major_enc = time_major_enc
         self.time_major_dec = time_major_dec
@@ -139,7 +173,6 @@ class TextIterator(object):
 
         self.source_buffer = list()
         self.target_buffer = list()
-        self.maxibatch_overflow = list()
         self.batch_overflow = list()
 
         self.end_of_data = False
@@ -175,27 +208,17 @@ class TextIterator(object):
 
         source_batch = list()
         target_batch = list()
-
         source_buffer_size = 0
-
-        longest_source = 0
-        longest_target = 0
 
         # Fill buffer if empty
         assert len(self.source_buffer) == len(self.target_buffer), 'Buffer size mismatch!'
 
         if len(self.source_buffer) == 0:
-
-            # Carry over mini-batch overflow
-            try:
-                overflow_items = self.maxibatch_overflow.pop()
-                self.source_buffer.append(overflow_items[0])
-                self.target_buffer.append(overflow_items[1])
-            except IndexError:
-                pass
-
+            lines_read = 0
             try:
                 for source_sentence in self.source:
+                    lines_read += 1
+
                     source_sentence = source_sentence.split()
                     target_sentence = self.target.readline().split()
 
@@ -203,19 +226,23 @@ class TextIterator(object):
                     if self.skip_empty and (len(source_sentence) == 0 or len(target_sentence) == 0):
                         continue
                     # Skip lines exceeding the maximum length
-                    if self.max_len > 0 and (len(source_sentence) > self.max_len or len(target_sentence) > self.max_len):
+                    if self.max_len > 0 and \
+                            (len(source_sentence) > self.max_len or len(target_sentence) > self.max_len):
                         continue
+                    # Update buffer
+                    self.source_buffer.append(source_sentence)
+                    self.target_buffer.append(target_sentence)
                     # Maxi-batching
                     source_buffer_size = source_buffer_size + len(source_sentence) if self.token_batches \
                         else source_buffer_size + 1
-                    # Update buffer
-                    if source_buffer_size <= self.maxibatch_size:
-                        self.source_buffer.append(source_sentence)
-                        self.target_buffer.append(target_sentence)
-                    else:
-                        # Prevent line dropping
-                        self.maxibatch_overflow.append((source_sentence, target_sentence))
+
+                    # Check if all lines have been read
+                    if lines_read == self.num_lines:
+                        self.all_lines_read = True
+
+                    if source_buffer_size >= self.maxibatch_size:
                         break
+
             except IOError:
                 self.end_of_data = True
 
@@ -240,18 +267,31 @@ class TextIterator(object):
                 self.source_buffer.reverse()
                 self.target_buffer.reverse()
 
-        # Carry over batch overflow
-        try:
-            overflow_items = self.batch_overflow.pop()
-            source_batch.append(overflow_items[0])
-            target_batch.append(overflow_items[1])
-        except IndexError:
-            pass
+            # Update current max-batch size
+            self.curr_maxibatch_size = source_buffer_size
+
+        # Actual work here
+        # Note: <EOS> id = 0, <GO> id = 1, <UNK> id = 2
+        # Track spill-over items and remaining GPUs
+        spillover = False
+        gpus_left = 0
 
         while True:
+            # Check if batch size needs to be adjusted to distribute the remaining maxi-batch items among all GPUs
+            curr_batch_size = self.batch_size
+            if self.config.num_gpus > 1 and self.all_lines_read:
+                gpus_left = self.config.num_gpus - (self.gpu_tracker % self.config.num_gpus)
+                if self.curr_maxibatch_size // (gpus_left * curr_batch_size) == 0:
+                    # Distribute load
+                    curr_batch_size = int(np.ceil(self.curr_maxibatch_size / gpus_left))
+
             # Read from source file and map to word index
             try:
                 source_sentence = self.source_buffer.pop()
+                # Decrement the maxi-batch size
+                self.curr_maxibatch_size = self.curr_maxibatch_size - len(source_sentence) if self.token_batches \
+                    else self.curr_maxibatch_size - 1
+
                 source_indices = list()
                 for element in source_sentence:
                     # Account for factored inputs
@@ -269,46 +309,49 @@ class TextIterator(object):
                 source_batch.append(source_indices)
                 target_batch.append(target_indices)
 
-                longest_source = max(longest_source, len(source_indices))
-                longest_target = max(longest_target, len(target_indices))
+                longest_source = max([len(item) for item in source_batch])
+                longest_target = max([len(item) for item in target_batch])
 
-                if self.token_batches:
-                    if len(source_batch) * longest_source > self.batch_size or \
-                            len(target_batch) * longest_target > self.batch_size:
-                        # Place last batched sentence into the overflow buffer to minimize data loss
-                        self.batch_overflow.append((source_batch.pop(), target_batch.pop()))
+                effective_source_batch_size = \
+                    len(source_batch) * longest_source if self.token_batches else len(source_batch)
+                effective_target_batch_size = \
+                    len(target_batch) * longest_target if self.token_batches else len(target_batch)
 
-                        if len(self.source_buffer) == 0:
-                            # Carry over batch overflow
-                            try:
-                                overflow_items = self.batch_overflow.pop()
-                                source_batch.append(overflow_items[0])
-                                target_batch.append(overflow_items[1])
-                            except IndexError:
-                                pass
-                        break
-                else:
-                    if len(source_batch) >= self.batch_size or len(target_batch) >= self.batch_size:
+                if (effective_source_batch_size > curr_batch_size or effective_target_batch_size > curr_batch_size) \
+                        and len(source_batch) > 1:
+                    # Avoid dropping spillover maxi-batch items
+                    if gpus_left == 1 and len(self.source_buffer) < self.config.num_gpus and self.all_lines_read:
+                        spillover = True
 
-                        if len(self.source_buffer) == 0:
-                            # Carry over batch overflow
-                            try:
-                                overflow_items = self.batch_overflow.pop()
-                                source_batch.append(overflow_items[0])
-                                target_batch.append(overflow_items[1])
-                            except IndexError:
-                                pass
-                        break
+                        # Add the spillover items to the current batch
+                        if len(self.source_buffer) != 0:
+                            continue
+
+                    if not spillover:
+                        # Push last batched sentence onto the batch overflow stack to avoid data loss
+                        source_batch.pop()
+                        target_batch.pop()
+                        self.batch_overflow.append((source_sentence, target_sentence))
+                    break
+
             except IndexError:
-
-                # Carry over batch overflow
-                try:
-                    overflow_items = self.batch_overflow.pop()
-                    source_batch.append(overflow_items[0])
-                    target_batch.append(overflow_items[1])
-                except IndexError:
-                    pass
                 break
+
+        # Track available GPUs
+        if self.config.num_gpus > 1:
+            self.gpu_tracker += 1
+
+        # Push batch overflow items back onto the maxi-batch
+        try:
+            overflow_items = self.batch_overflow.pop()
+            self.source_buffer += [overflow_items[0]]
+            self.target_buffer += [overflow_items[1]]
+            # Increment maxi-batch size
+            self.curr_maxibatch_size = self.curr_maxibatch_size + len(overflow_items[0]) \
+                if self.token_batches else self.curr_maxibatch_size + 1
+
+        except IndexError:
+            pass
 
         return source_batch, target_batch
 
@@ -325,16 +368,11 @@ class TextIterator(object):
         # Fill buffer if empty
         if len(self.source_buffer) == 0:
 
-            # Carry over mini-batch overflow
-            try:
-                self.source_buffer.append(self.maxibatch_overflow.pop())
-            except IndexError:
-                pass
-
             try:
                 for source_sentence in self.source:
-                    source_sentence = source_sentence.split()
+                    lines_read = 0
 
+                    source_sentence = source_sentence.split()
                     # Skip empty lines
                     if self.skip_empty and len(source_sentence) == 0:
                         continue
@@ -342,12 +380,15 @@ class TextIterator(object):
                     source_buffer_size = source_buffer_size + len(source_sentence) if self.token_batches \
                         else source_buffer_size + 1
                     # Update buffer
-                    if source_buffer_size <= self.maxibatch_size:
-                        self.source_buffer.append(source_sentence)
-                    else:
-                        # Prevent line dropping
-                        self.maxibatch_overflow.append(source_sentence)
+                    self.source_buffer.append(source_sentence)
+
+                    # Check if all lines have been read
+                    if lines_read == self.num_lines:
+                        self.all_lines_read = True
+
+                    if source_buffer_size >= self.maxibatch_size:
                         break
+
             except IOError:
                 self.end_of_data = True
 
@@ -364,16 +405,29 @@ class TextIterator(object):
             else:
                 self.source_buffer.reverse()
 
-        # Carry over batch overflow
-        try:
-            source_batch.append(self.batch_overflow.pop())
-        except IndexError:
-            pass
+            # Update current max-batch size
+            self.curr_maxibatch_size = source_buffer_size
+
+        # Actual work here
+        # Note: <EOS> id = 0, <GO> id = 1, <UNK> id = 2
+        # Track spill-over items and remaining GPUs
+        spillover = False
+        gpus_left = 0
 
         while True:
-            # Read from source file and map to word index
+            # Check if batch size needs to be adjusted to distribute the remaining maxi-batch items among all GPUs
+            curr_batch_size = self.batch_size
+            if self.config.num_gpus > 1 and self.all_lines_read:
+                gpus_left = self.config.num_gpus - (self.gpu_tracker % self.config.num_gpus)
+                if self.curr_maxibatch_size // (gpus_left * curr_batch_size) == 0:
+                    # Distribute load
+                    curr_batch_size = int(np.ceil(self.curr_maxibatch_size / gpus_left))
             try:
                 source_sentence = self.source_buffer.pop()
+                # Decrement the maxi-batch size
+                self.curr_maxibatch_size = self.curr_maxibatch_size - len(source_sentence) if self.token_batches \
+                    else self.curr_maxibatch_size - 1
+
                 source_indices = list()
                 for element in source_sentence:
                     # Account for factored inputs
@@ -387,38 +441,38 @@ class TextIterator(object):
                 source_batch.append(source_indices)
                 longest_source = max(longest_source, len(source_indices))
 
-                if self.token_batches:
-                    if len(source_batch) * longest_source >= self.batch_size:
-                        # Place last batched sentence into the overflow buffer to minimize data loss
+                effective_source_batch_size = \
+                    len(source_batch) * longest_source if self.token_batches else len(source_batch)
+
+                if effective_source_batch_size > curr_batch_size and len(source_batch) > 1:
+                    # Avoid dropping spillover maxi-batch items
+                    if gpus_left == 1 and len(self.source_buffer) < self.config.num_gpus and self.all_lines_read:
+                        spillover = True
+                        # Add the spillover items to the current batch
+                        if len(self.source_buffer) != 0:
+                            continue
+
+                    if not spillover:
+                        # Push last batched sentence onto the batch overflow stack to avoid data loss
                         self.batch_overflow.append(source_batch.pop())
+                    break
 
-                        if len(self.source_buffer) == 0:
-                            # Carry over batch overflow
-                            try:
-                                source_batch.append(self.batch_overflow.pop())
-                            except IndexError:
-                                pass
-
-                        break
-                else:
-                    if len(source_batch) >= self.batch_size:
-
-                        if len(self.source_buffer) == 0:
-                            # Carry over batch overflow
-                            try:
-                                source_batch.append(self.batch_overflow.pop())
-                            except IndexError:
-                                pass
-
-                        break
             except IndexError:
-
-                # Carry over batch overflow
-                try:
-                    source_batch.append(self.batch_overflow.pop())
-                except IndexError:
-                    pass
                 break
+
+            # Track available GPUs
+            if self.config.num_gpus > 1:
+                self.gpu_tracker += 1
+
+            # Push batch overflow items back onto the maxi-batch
+            try:
+                popped_source = self.batch_overflow.pop()
+                self.source_buffer += popped_source
+                # Increment maxi-batch size
+                self.curr_maxibatch_size = self.curr_maxibatch_size + len(popped_source) \
+                    if self.token_batches else self.curr_maxibatch_size + 1
+            except IndexError:
+                pass
 
         return source_batch
 
@@ -483,7 +537,7 @@ class TextIterator(object):
                 target_out[sentence_id, :target_lengths[sentence_id]] = target_sentence
                 target_mask[sentence_id, :target_lengths[sentence_id] + 1] = 1.
 
-        # TODO: Remove hack once factored inputs are required (and adjust rest of code)
+        # TODO: Remove hack once factored inputs are supported (and adjust rest of code)
         source = np.squeeze(source, axis=0)
         return source, target_in, target_out, source_mask, target_mask
 
@@ -527,7 +581,7 @@ class TextIterator(object):
                 source[:, sentence_id, :source_lengths[sentence_id]] = list(zip(*source_sentence))
                 source_mask[sentence_id, :source_lengths[sentence_id] + 1] = 1.
 
-        # TODO: Remove hack once factored inputs are required (and adjust rest of code)
+        # TODO: Remove hack once factored inputs are supported (and adjust rest of code)
         source = np.squeeze(source, axis=0)
 
         # Add dummy placeholders

codebase/inference.py
@@ -5,11 +5,11 @@ from layers import get_shape_list
 """ Inference functions for the transformer model. The generative process follows the 'Look, Generate, Update' paradigm, 
 as opposed to the 'Look, Update, Generate' paradigm employed by the deep-RNN. """
 
-
 # TODO: Add coverage penalty from Tu, Zhaopeng, et al.
 # TODO: "Modeling coverage for neural machine translation." arXiv preprint arXiv:1601.04811 (2016).
 
-# Note: Some inference mechanisms are adopted from the tensor2tensor library, with modifications
+
+# Note: Some inference mechanisms are adopted from the tensor2tensor library, with moderate modifications
 
 # ============================================== Helper functions ==================================================== #
 
@@ -91,7 +91,6 @@ def gather_top_sequences(all_sequences,
     gathered_memories = None
     if all_memories is not None:
         gathered_memories = gather_memories(all_memories, gather_coordinates)
-        # gathered_memories = all_memories
 
     return gathered_sequences, gathered_scores, gathered_eos_flags, gathered_memories
 
@@ -135,7 +134,6 @@ def greedy_search(decoding_function,
         # Extend next_id's dimensions to be compatible with input dimensionality for the subsequent step
         next_ids = tf.expand_dims(next_ids, time_dim)
         # Check if generation has concluded with <EOS>
-        # all_finished |= tf.equal(tf.squeeze(next_ids, axis=time_dim), eos_id)
         all_finished |= tf.equal(tf.reduce_prod(decoded_ids - eos_id, axis=time_dim), eos_id)
 
         return current_time_step + 1, all_finished, next_ids, decoded_ids, decoded_score, memories

codebase/ops.py
@@ -1,5 +1,8 @@
+import copy
+
 import tensorflow as tf
 from util import get_visible_gpus, assign_to_device, get_devices
+
 
 # TODO: Currently, translation does not work with multiple-GPUs
 
@@ -7,6 +10,14 @@ from util import get_visible_gpus, assign_to_device, get_devices
 def get_parallel_ops(model, iterator, num_gpus, eos_id, mode, no_summaries=False):
     """ Defines the training and validation OPs for multi-GPU training.
     Vaguely based on: http://blog.s-schoener.com/2017-12-15-parallel-tensorflow-intro/ . """
+
+    def _pad_to_max_step_len(batch, max_step_len, is_3d=False):
+        """ Pads model translations to a uniform length as required for concatenation across towers. """
+        if is_3d:
+            padding = [[0, 0], [0, 0], [0, max_step_len - tf.shape(batch)[-1]]]
+        else:
+            padding = [[0, 0], [0, max_step_len - tf.shape(batch)[-1]]]
+        return tf.pad(batch, padding, 'constant', constant_values=eos_id)
 
     def _get_train_ops():
         """ Defines multi-device OPs used to train the model. """
@@ -25,6 +36,7 @@ def get_parallel_ops(model, iterator, num_gpus, eos_id, mode, no_summaries=False
                     with tf.device(assign_to_device(controller, gpu)), tf.name_scope(name):
                         # Compute and store losses and gradients
                         next_batch = iterator.get_next()
+
                         grads_and_vars, _, batch_loss, sentence_loss, words_processed, words_evaluated = \
                             model.train_model(next_batch)
                         # Training OPs
@@ -37,9 +49,10 @@ def get_parallel_ops(model, iterator, num_gpus, eos_id, mode, no_summaries=False
                     # Reuse variables
                     outer_scope.reuse_variables()
 
+                # TODO: Not sure if this does anything, as code is used at graph construction time
                 except tf.errors.OutOfRangeError:
                     break
-
+        # TODO: Same here
         if len(tower_grads_and_vars) == 0:
             raise tf.errors.OutOfRangeError
 
@@ -58,9 +71,9 @@ def get_parallel_ops(model, iterator, num_gpus, eos_id, mode, no_summaries=False
                 averaged_grad = tf.reduce_mean(grads, 0)
             else:
                 # Concatenate IndexedSlices (equivalent to averaging of tensors)
-                # Apply tower weights
                 values = [grads[tower_id].values * tower_weights[tower_id] for tower_id in range(len(grads))]
                 joint_values = tf.concat(values, axis=0)
+
                 joint_indices = tf.concat([grad.indices for grad in grads], axis=0)
                 averaged_grad = \
                     tf.IndexedSlices(values=joint_values, indices=joint_indices, dense_shape=grads[0].dense_shape)
@@ -80,6 +93,97 @@ def get_parallel_ops(model, iterator, num_gpus, eos_id, mode, no_summaries=False
         # Proto-OPs are forwarded to gradient accumulation or optimization
         return proto_train_ops
 
+    def _get_multi_valid_ops():
+        """ Defines multi-device OPs used to evaluate the model; used for the external validation step only.
+        CURRENTLY UNUSED. """
+        # TODO: DEPRECATED!
+        # Track tower-wise outputs
+        tower_batch_losses = list()
+        tower_sentence_losses = list()
+        tower_words = list()
+
+        with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE) as outer_scope:
+            for gpu_id, gpu in enumerate(operators):
+                name = 'tower_{}'.format(gpu_id)
+                # Assign variables to the CPU and tensor OPs to GPUs
+                with tf.device(assign_to_device(controller, gpu)), tf.name_scope(name):
+                    # Compute and store losses and gradients
+                    next_batch = iterator.get_next()
+                    _, _, _, batch_loss, sentence_loss, words_processed = model.train_model(next_batch)
+
+                    # Training/ validation OPs
+                    tower_batch_losses.append(batch_loss)
+                    tower_words.append(words_processed)
+                    tower_sentence_losses.append(sentence_loss)
+
+                # Reuse variables
+                outer_scope.reuse_variables()
+
+            # Merged validation OPs
+            averaged_batch_loss = tf.reduce_mean(tower_batch_losses)
+            joint_sentence_losses = tf.concat(tower_sentence_losses, axis=0)
+            total_words_processed = tf.reduce_sum(tower_words)
+            valid_ops = [averaged_batch_loss, joint_sentence_losses, total_words_processed]
+        return valid_ops
+
+    def _get_valid_ops():
+        """ Defines single-device OPs used to evaluate the model; used for the external validation step only. """
+        # TODO: DEPRECATED!
+        with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE):
+            with tf.device(assign_to_device(controller, operators[0])):
+                # Validation OPs
+                next_batch = iterator.get_next()
+
+                _, _, _, batch_loss, sentence_loss, words_processed = model.train_model(next_batch)
+
+        valid_ops = [batch_loss, sentence_loss, words_processed]
+        return valid_ops
+
+    def _get_multi_translation_ops():
+        """ Defines multi-device OPs used to obtain translations from the model. CURRENTLY UNUSED. """
+        # Track tower-wise outputs
+        tower_greedy_translations = list()
+        tower_sampled_translations = list()
+        tower_beam_translations = list()
+        tower_beam_scores = list()
+
+        with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE) as outer_scope:
+            for gpu_id, gpu in enumerate(operators):
+                name = 'tower_{}'.format(gpu_id)
+                # Assign variables to the CPU and tensor OPs to GPUs
+                with tf.device(assign_to_device(controller, gpu)), tf.name_scope(name):
+                    # Translation OPs (output has to be padded to same size before concatenation)
+                    next_batch = iterator.get_next()
+                    greedy_translations, _, _ = model.decode_greedy(next_batch)
+                    sampled_translations, _ = model.decode_with_sampling(next_batch)
+                    beam_translations, beam_scores = model.decode_with_beam_search(next_batch)
+
+                    tower_greedy_translations.append(greedy_translations)
+                    tower_sampled_translations.append(sampled_translations)
+                    tower_beam_translations.append(beam_translations)
+                    tower_beam_scores.append(beam_scores)
+                # Reuse variables
+                outer_scope.reuse_variables()
+
+        with tf.name_scope('translation'), tf.device('/cpu:0'):
+            # Merged translation OPs
+            greedy_mst = tf.reduce_max([tf.shape(batch)[-1] for batch in tower_greedy_translations])
+            sampled_mst = tf.reduce_max([tf.shape(batch)[-1] for batch in tower_sampled_translations])
+            beam_mst = tf.reduce_max([tf.shape(batch)[-1] for batch in tower_beam_translations])
+
+            padded_greedy_translators = [_pad_to_max_step_len(batch, greedy_mst) for batch in tower_greedy_translations]
+            padded_sampled_translators = [_pad_to_max_step_len(batch, sampled_mst) for batch in
+                                          tower_sampled_translations]
+            padded_beam_translators = [_pad_to_max_step_len(batch, beam_mst, True) for batch in tower_beam_translations]
+
+            joint_greedy_translators = tf.concat(padded_greedy_translators, axis=0)
+            joint_sampled_translators = tf.concat(padded_sampled_translators, axis=0)
+            joint_beam_translators = tf.concat(padded_beam_translators, axis=0)
+            joint_beam_scores = tf.concat(tower_beam_scores, axis=0)
+            translation_ops = \
+                [joint_greedy_translators, joint_sampled_translators, joint_beam_translators, joint_beam_scores]
+        return translation_ops
+
     def _get_translation_ops():
         """ Defines single-device OPs used to obtain translations from the model. """
         with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE):
@@ -94,8 +198,8 @@ def get_parallel_ops(model, iterator, num_gpus, eos_id, mode, no_summaries=False
                            beam_translations, beam_scores]
         return translation_ops
 
-    assert mode in ['training', 'validation', 'translation'], \
-        'Specified OP-retrieval mode must be training, validation, or translation'
+    assert mode in ['training', 'translation'], \
+        'Specified OP-retrieval mode must be training or translation'
 
     # Detect available GPUs
     controller, operators = get_devices(num_gpus)
@@ -123,6 +227,17 @@ def get_single_ops(model, iterator, num_gpus, unused_eos_id, mode, no_summaries=
             proto_train_ops.append(summaries)
         return proto_train_ops
 
+    def _get_valid_ops(next_batch):
+        """ Defines single-device OPs used to evaluate the model; used for the external validation step only. """
+        # TODO: DEPRECATED!
+        with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE):
+            with tf.device(assign_to_device(controller, operators[0])):
+                # Surface OPs
+                _, _, _, batch_loss, sentence_loss, words_processed = model.train_model(next_batch)
+
+        valid_ops = [batch_loss, sentence_loss, words_processed]
+        return valid_ops
+
     def _get_translation_ops(next_batch):
         """ Defines single-device OPs used to obtain translations from the model. """
         with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE):
@@ -136,7 +251,8 @@ def get_single_ops(model, iterator, num_gpus, unused_eos_id, mode, no_summaries=
                            beam_translations, beam_scores]
         return translation_ops
 
-    assert mode in ['training', 'translation'], 'Specified OP-retrieval mode must be training or translation'
+    assert mode in ['training', 'translation'], \
+        'Specified OP-retrieval mode must be training or translation'
 
     # Detect available GPUs
     controller, operators = get_devices(num_gpus)
@@ -154,18 +270,34 @@ class VariableUpdateTrainer(object):
     """ Class for training models with variable gradient aggregation updates;
     Inspired by the Adam-specific gradient aggregation implementation by fstahlberg@github
     (https://github.com/fstahlberg/tensor2tensor/blob/master/tensor2tensor/utils/largebatch_optimizer.py) """
-    def __init__(self, model, num_layers, iterator, num_gpus, eos_id, n_agg_steps, use_multi_gpu, session):
+
+    def __init__(self,
+                 model,
+                 num_layers,
+                 iterator,
+                 num_gpus,
+                 eos_id,
+                 n_agg_steps,
+                 warmup_steps,
+                 use_multi_gpu,
+                 session,
+                 track_grad_rates=False,
+                 grad_norm_threshold=0.0):
+
         self.model = model
         self.num_layers = num_layers
         self.iterator = iterator
         self.num_gpus = num_gpus
         self.eos_id = eos_id
         self.n_agg_steps = n_agg_steps if n_agg_steps > 0 else 1
+        self.warmup_steps = warmup_steps
         self.use_multi_gpu = use_multi_gpu
         self.session = session
+        self.track_grad_rates = track_grad_rates
+        self.grad_norm_threshold = grad_norm_threshold
 
         forward_fn = get_parallel_ops if use_multi_gpu else get_single_ops
-        self.train_ops = forward_fn(model, iterator, num_gpus, eos_id, mode='training')
+        self.train_ops = forward_fn(model, iterator, num_gpus, eos_id, mode='training', no_summaries=True)
 
         self.grads_cache = None
         self.batch_loss_cache = None
@@ -173,6 +305,7 @@ class VariableUpdateTrainer(object):
 
         self.optimizer = model.optimizer
         self.curr_agg_step = 0
+        self.curr_global_step = tf.Variable(tf.constant(0), trainable=False)
         self.do_update = False
 
         with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE) as outer_scope:
@@ -199,24 +332,84 @@ class VariableUpdateTrainer(object):
         zero_words_processed = tf.assign(self.words_processed_cache, self.words_processed_cache * 0)
         return tf.group(zero_grads, zero_batch_loss, zero_words_processed)
 
-    def _get_grad_norm_ratio(self, t_vars, grads):
-        """ Computes the grad norm ratio, as introduced in
+    def _get_grad_norm_ratios(self, t_vars, grads):
+        """ Computes grad-norm-ratios and parameter-grad-ratios, as introduced in
         Bapna, Ankur, et al. "Training Deeper Neural Machine Translation Models with Transparent Attention.",
-        arXiv preprint arXiv:1808.07561 (2018). """
-        initial_grads = list()
-        final_grads = list()
+        arXiv preprint arXiv:1808.07561 (2018) and
+        You, Y., I. Gitman, and B. Ginsburg. "Large batch training of convolutional networks."
+        ArXiv e-prints (2017), respectively."""
+        # Set up gradient tracking
+        grad_dict = dict()
+        sub_networks = ['encoder', 'decoder']
+        tracked_layers = ['layer_{:d}'.format(layer_id) for layer_id in range(1, self.num_layers + 1)]
+        final_layer = tracked_layers[-1]
+        for sn in sub_networks:
+            grad_dict[sn] = dict()
+            for tl in tracked_layers:
+                grad_dict[sn][tl] = list()
+
+        param_dict = copy.deepcopy(grad_dict)
+        grad_dict['embedding_table'] = list()
 
         # Compute average gradient for the initial and final layers
         for var_id, var in enumerate(t_vars):
-            if 'layer_1' in var.name:
-                initial_grads.append(tf.norm(grads[var_id], ord=2))
-            elif 'layer_{:d}'.format(self.num_layers) in var.name:
-                final_grads.append(tf.norm(grads[var_id], ord=2))
-            else:
-                pass
+            for sn in sub_networks:
+                if sn in var.name:
+                    for tl in tracked_layers:
+                        if tl in var.name:
+                            grad_dict[sn][tl].append(tf.norm(grads[var_id], ord=2))
+                            param_dict[sn][tl].append(tf.norm(var, ord=2))
 
-        # Compute grad norm ratio
-        return tf.reduce_mean(initial_grads) / tf.reduce_mean(final_grads)
+            if 'embedding_table' in var.name:
+                grad_dict['embedding_table'].append(tf.norm(grads[var_id], ord=2))
+
+        # Compute grad-norm-ratios
+        enc_layer_ratio = \
+            tf.reduce_mean(grad_dict['encoder']['layer_1']) / tf.reduce_mean(grad_dict['encoder'][final_layer])
+
+        if self.track_grad_rates:
+            enc_embed_ratio = \
+                tf.reduce_mean(grad_dict['embedding_table']) / tf.reduce_mean(grad_dict['encoder'][final_layer])
+            dec_embed_ratio = \
+                tf.reduce_mean(grad_dict['embedding_table']) / tf.reduce_mean(grad_dict['decoder'][final_layer])
+            dec_layer_ratio = \
+                tf.reduce_mean(grad_dict['decoder']['layer_1']) / tf.reduce_mean(grad_dict['decoder'][final_layer])
+
+            # Generate summaries
+            with tf.name_scope('grad_norm_ratio_summaries'):
+                enc_embed_ratio_summary = \
+                    tf.summary.scalar(name='encoder_embeddings_grad_norm_ratio', tensor=enc_embed_ratio)
+                enc_layer_ratio_summary = \
+                    tf.summary.scalar(name='encoder_layers_grad_norm_ratio', tensor=enc_layer_ratio)
+                dec_embed_ratio_summary = \
+                    tf.summary.scalar(name='decoder_embeddings_grad_norm_ratio', tensor=dec_embed_ratio)
+                dec_layer_ratio_summary = \
+                    tf.summary.scalar(name='decoder_layers_grad_norm_ratio', tensor=dec_layer_ratio)
+                # Merge
+                grad_norm_summaries = tf.summary.merge([enc_embed_ratio_summary, enc_layer_ratio_summary,
+                                                        dec_embed_ratio_summary, dec_layer_ratio_summary],
+                                                       name='grad_norm_ratio_summaries')
+
+            # Compute parameter-grad-ratios
+            with tf.name_scope('param_grad_ratio_summaries'):
+                param_grad_ratio_summaries = list()
+                for sn in sub_networks:
+                    for tl in tracked_layers:
+                        param_grad_ratio = tf.reduce_mean(param_dict[sn][tl]) / tf.reduce_mean(grad_dict[sn][tl])
+                        param_grad_ratio_name = '{:s}_{:s}_param_grad_ratio'.format(sn, tl)
+                        param_grad_ratio_summaries.append(
+                            tf.summary.scalar(name=param_grad_ratio_name, tensor=param_grad_ratio))
+                # Merge
+                with tf.name_scope('param_grad_ratio_summaries'):
+                    param_grad_ratio_summaries = \
+                        tf.summary.merge(param_grad_ratio_summaries, name='param_grad_ratio_summaries')
+            # Merge
+            grad_summaries = \
+                tf.summary.merge([grad_norm_summaries, param_grad_ratio_summaries], name='grad_summaries')
+        else:
+            grad_summaries = None
+
+        return enc_layer_ratio, grad_summaries
 
     def _update(self):
         """ Updates gradients and metrics. """
@@ -233,7 +426,7 @@ class VariableUpdateTrainer(object):
 
         # Aggregate
 
-        # Sophisticated gradient monitoring
+        # 'Sophisticated' gradient monitoring
         for grad_id, step_grad in enumerate(step_grads):
             if step_grad is None:
                 print(t_vars[grad_id])
@@ -254,7 +447,7 @@ class VariableUpdateTrainer(object):
         # Grad norm ratio
         grad_norm_ratio_op = tf.no_op()
         # Define summaries
-        summaries = self.model.get_summaries(batch_loss)
+        summaries = tf.no_op()
         return [batch_loss, words_processed, train_op, grad_norm_ratio_op, summaries]
 
     def _update_and_apply(self):
@@ -263,14 +456,26 @@ class VariableUpdateTrainer(object):
         t_vars, grads, batch_loss, words_processed = self._update()
         # Normalize
         grads = [tf.assign(grad, grad / self.n_agg_steps) for grad in grads]
+        # Optionally clip gradients after the warm-up phase concluded
+        grads = tf.cond(tf.logical_and(tf.greater(self.grad_norm_threshold, 0.0),
+                                       tf.greater_equal(self.curr_global_step, self.warmup_steps)),
+                        lambda: [tf.clip_by_norm(grad, self.grad_norm_threshold) for grad in grads],
+                        lambda: grads)
+
         # Define train OP
         grads_and_vars = [(grad, t_vars[grad_id]) for grad_id, grad in enumerate(grads)]
         train_op = self.optimizer.apply_gradients(grads_and_vars, global_step=self.model.global_step)
+        # Update trainer-internal global step
+        gs_update = tf.assign_add(self.curr_global_step, 1)
         # Grad norm ratio
-        grad_norm_ratio_op = self._get_grad_norm_ratio(t_vars, grads)
-        # Define summaries
+        grad_norm_ratio_op, grad_summaries = self._get_grad_norm_ratios(t_vars, grads)
+        # Define model summaries
         summaries = self.model.get_summaries(batch_loss)
-        return [batch_loss / self.n_agg_steps, words_processed, train_op, grad_norm_ratio_op, summaries]
+        if grad_summaries is not None:
+            # Merge with grad norm ratio summaries
+            summaries = tf.summary.merge([summaries, grad_summaries], name='trainer_summaries')
+        return [batch_loss / self.n_agg_steps, words_processed, tf.group(train_op, gs_update), grad_norm_ratio_op,
+                summaries]
 
     def forward(self):
         with tf.name_scope('optimization'), tf.device('/cpu:0'):

codebase/transformer.py
@@ -24,7 +24,6 @@ class Transformer(object):
         self.float_dtype = tf.float32
 
         with tf.name_scope('{:s}_inputs_and_variables'.format(self.name)), tf.device('/cpu:0'):
-            # TODO: Revise device assignment once Data API supports GPU processing
             # Declare placeholders
             self.learning_rate = tf.placeholder(dtype=self.float_dtype, name='adaptive_learning_rate', shape=[])
             # Sampling epsilon placeholder for use with hybrid models
@@ -210,13 +209,15 @@ class TransformerEncoder(object):
                  embedding_layer,
                  training,
                  float_dtype,
-                 name):
+                 name,
+                 to_rnn=False):
         # Set attributes
         self.config = config
         self.embedding_layer = embedding_layer
         self.training = training
         self.float_dtype = float_dtype
         self.name = name
+        self.to_rnn = to_rnn  # Denotes whether the encoder is connected to an RNN decoder
 
         # Track layers
         self.encoder_stack = dict()
@@ -264,14 +265,19 @@ class TransformerEncoder(object):
             source_embeddings = self._embed(source_ids)
             # Obtain length and depth of the input tensors
             _, time_steps, depth = get_shape_list(source_embeddings)
-            # Transform input mask into attention mask
-            inverse_mask = tf.cast(tf.equal(source_mask, 0.0), dtype=self.float_dtype)
-            attn_mask = inverse_mask * -1e9
-            # Expansion to shape [batch_size, 1, 1, time_steps] is needed for compatibility with attention logits
-            attn_mask = tf.expand_dims(tf.expand_dims(attn_mask, 1), 1)
-            # Differentiate between self-attention and cross-attention masks for further, optional modifications
-            self_attn_mask = attn_mask
-            cross_attn_mask = attn_mask
+            if not self.to_rnn:
+                # Transform input mask into attention mask
+                inverse_mask = tf.cast(tf.equal(source_mask, 0.0), dtype=self.float_dtype)
+                attn_mask = inverse_mask * -1e9
+                # Expansion to shape [batch_size, 1, 1, time_steps] is needed for compatibility with attention logits
+                attn_mask = tf.expand_dims(tf.expand_dims(attn_mask, 1), 1)
+                # Differentiate between self-attention and cross-attention masks for further, optional modifications
+                self_attn_mask = attn_mask
+                cross_attn_mask = attn_mask
+            else:
+                # No mask manipulation required for the RNN decoder
+                self_attn_mask = None
+                cross_attn_mask = source_mask
             # Add positional encodings
             positional_signal = get_positional_signal(time_steps, depth, self.float_dtype)
             source_embeddings += positional_signal